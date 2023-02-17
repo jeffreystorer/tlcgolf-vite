c6947b537a1332f99ec876d73302b42635d4578e
jeffreystorer <jeffrey.storer@gmail.com>
2023-02-17 12:29:14 -0500

    2/17 Add Saturday Page

---
+++ src/components/saturday/SaturdayTable.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SaturdayTable({ isLoggedIn }) {
+  console.log('😊😊 isLoggedIn', isLoggedIn);
+  return (
+    <>
+      <h1>SaturdayTable</h1>
+    </>
+  );
+}
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 //import preval from 'preval.macro';
 import FetchData from '@/components/fetchdata/FetchData';
+//import FetchDataSaturday from '@/components/fetchdata/FetchDataSaturday';
 import SignIn from '@/components/signin/SignIn';
 import capitalize from '@/utils/capitalize';
 import { get, set } from '@/utils/localStorage';
